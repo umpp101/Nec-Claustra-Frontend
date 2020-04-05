fadfdb9813bcbf9ccb6fa245019ab0a50ee0d1b3
Mustafa Muse <umpp101@icloud.com>
2020-04-05 13:01:17 -0700

    search

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -11,13 +11,19 @@ export default class Inbox extends Component {
     super(props)
   
     this.state = {
-       searchedUsers: []
+       searchedUsers: [],
+       searchedTerm: ''
     }
   }
 
   searchForUsers = (event) => {
+
     console.log(event.target.value)
-    let searchResult = this.props.allUsers.filter(user =>user.user_name.includes(event.target.value));
+    this.setState({
+      searchedTerm: event.target.value
+    })
+    // console.log(event.target.value)
+    let searchResult = event.target.value !== ''? this.props.allUsers.filter(user =>user.user_name.includes(event.target.value)) : [];
     console.log(searchResult)
     this.setState({
       searchedUsers: searchResult
@@ -28,6 +34,7 @@ export default class Inbox extends Component {
     if (Object.keys(this.props.currentUser).length !== 0) {
       this.props.openWsConnection();
     }
+    console.log(Object.keys(this.props.currentConvo).length === 0)
   }
 
   render() {
@@ -37,27 +44,46 @@ export default class Inbox extends Component {
         <div className="people-list" id="people-list">
           <Search 
           allUsers={this.props.allUsers}
-          searchForUsers={this.searchForUsers}/>
+          searchForUsers={this.searchForUsers}
+          searchedTerm={this.state.searchedTerm}
+          searchedUsers={this.state.searchedUsers}
+          handleNewConvo={this.props.handleNewConvo}/>
+          
           {this.props.myConvos.map(convo =>
             <PeopleList
+                
                 getUserNameById={this.props.getUserNameById}
                 setConvo={this.props.setConvo}
                 allUsers={this.props.allUsers}
                 convo={convo} key={convo.id}
                 currentUser={this.props.currentUser} />)}
         </div>
+
+
+
         <div className="chat">
+          
+        {Object.keys(this.props.currentConvo).length === 0 ? 
+        <div className="chat-history">
+          <h1> Please choose a user to chat with.. </h1>
+        </div>
+          :
+          <>
           <CurrentChatHeader
+            getUserNameById={this.props.getUserNameById}
             currentConvo={this.props.currentConvo}
-          />
-          <div className="chat-history">
+            currentUser={this.props.currentUser}
+            allUsers={this.props.allUsers} />
+         <div className="chat-history">
            <Messages
               currentConvo={this.props.currentConvo}
               currentUser={this.props.currentUser} />
           </div>
           <NewMessage
             handleSendEvent={this.props.handleSendEvent} />
-        </div>
+          </>
+          }
+        </div> 
       </div>
 
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Search(props) {
-    return (
-        <div className="search">
-        <input type="text" placeholder="search" 
-        onChange={(event) => props.searchForUsers(event)}/>
-        <i className="fa fa-search"></i>
-    </div>
-    )
+
+export class Search extends Component {
+ 
+
+
+    mySuggestions = () => {
+        if (this.props.searchedUsers.length !== 0) {
+            return this.props.searchedUsers.map(user => {
+
+                return (
+                    <div className="clearfix">
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg" alt="avatar" className="about" />
+                        <div className="about" >
+                            <div className="name" onClick={(e) => this.props.handleNewConvo(e,user)}>{user.user_name}</div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+        else if (this.props.searchedTerm !== "") {
+            return (
+                <p> `{this.props.searchedTerm}  .. is a user that does'nt exists` </p>
+            )
+        }
+    }
+    render() {
+        return (
+
+            <div className="search">
+                <input type="text" placeholder="search"
+                    onChange={(event) => this.props.searchForUsers(event)} />
+                {this.mySuggestions()}
+            </div>
+        )
+    }
 }
 
 export default Search
+
 
