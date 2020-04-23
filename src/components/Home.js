18496abede6d509251ac54e10ddb1d4429add356
Mustafa Muse <umpp101@icloud.com>
2020-04-23 15:56:03 -0700

    home functional

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export class Home extends Component {
-  render() {
-    return (
-      <div class="css-typing">
-      <h2>
-        Welcome to Nec Claustra!
-      </h2>
+function Home() {
+  return (
+    <div class="css-typing">
+      <h2>Welcome to Nec Claustra!</h2>
       <h2>
         Ever wanted to have a chat with someone that spoke another language?
-     </h2>
-      <h2>
-        Only to realize it would be a hassle going back and forth translating their words?
       </h2>
       <h2>
-        I've been there before! and here is a simple work around to help solve that problem...
+        Only to realize it would be a hassle going back and forth translating
+        their words?
       </h2>
       <h2>
-        Chat with no language barriers!
+        I've been there before! and here is a simple work around to help solve
+        that problem...
       </h2>
+      <h2>Chat with no language barriers!</h2>
     </div>
-    )
-  }
+  );
 }
-
-export default Home
+export default Home;
